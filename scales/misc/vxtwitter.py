04cacdfbb0d474501fdaa284049fa5c4bf21fbff
kajdev <zekewotring@gmail.com>
2022-11-19 21:16:46 -0900

    fix

--- scales/misc/vxtwitter.py
+++ scales/misc/vxtwitter.py
@@ -9,7 +9,7 @@ twitter_link_regex = re.compile(r'https?:\/\/(?:.*\.)?(?:twitter\.com|t\.co)\/(?
 class Vxtwitter(Extension):
 
     @listen()
-    async def on_message_update(self, event: MessageUpdate):
+    async def on_message_create(self, event: MessageCreate):
         message = event.after
 
         if message.author.bot:
@@ -19,11 +19,23 @@ class Vxtwitter(Extension):
         twitter_link = re.search(twitter_link_regex, message.content or "")
 
         # fetch the message if there is one
-        if twitter_link and any([bool(embed.video) for embed in message.embeds]):
+        if twitter_link:
             guild_stuff: models.Guild = await self.bot.db.fetch_guild(message.guild.id)
 
             if not guild_stuff.module_enabled(models.ModuleToggles.FXTWITTER):
                 return
+
+            fxt_link = twitter_link.group().replace("twitter.com", "fxtwitter.com")
+
+            # fetch fxtwitter
+            async with aiohttp.ClientSession() as session:
+                async with session.get(fxt_link) as resp:
+                    if resp.status != 200:
+                        return
+                    data = await resp.text()
+
+                    if '<meta property="twitter:player:stream:content_type" content="video/mp4"/>' not in data:
+                        return
 
             # clear the embeds
             await message.suppress_embeds()
